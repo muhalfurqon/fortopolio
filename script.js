cf8103599789b387a491044e88b8169c653e2171
muhalfurqon <161838612+muhalfurqon@users.noreply.github.com>
2024-03-03 14:12:41 +0700

    ubah

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// created by : RENOLD ALVAN FUADY
-// created on : 25-01-2022
-// NIM        : 10121244 IF-7
-// copyright RAF 
 
-// Navbar
 var nav = document.querySelector('nav');
 window.addEventListener('scroll', function () {
     if (window.pageYOffset > 150) {
@@ -12,5 +7,4 @@ window.addEventListener('scroll', function () {
         nav.classList.remove('navbar-scroll');
     }
 });
-// End Navbar
 
